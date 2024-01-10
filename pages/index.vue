67e0578fdeaacf20532eed5b7f61d44484e6be2a
burmaob <156067621+burmaob@users.noreply.github.com>
2024-01-10 13:16:13 +0000

    add logo to homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,10 @@
 <template>
   <main>
     <section class="self-center flex flex-col flex-1 items-center justify-center">
+      <img src="icon.png" alt="logo" width="500" height="500">
       <h1 class="title text-center">ဘားမားအွန်လိုင်းဘတ်တင်</h1>
       <h2 class="subtitle text-center">ibet789, Sports899, 555mix, Shan234 များကို စိတ်ချယုံကြည်စွာနဲ့ ကစားနိုင်ပါတယ်</h2>
-      <h3 class="subtitle text-center">မလောင်းတတ် မကစားတတ်လည်း စိတ်ရှည်လက်ရှည် ရှင်းပြပေးမှာမလို့ 09-751290444 ကို ၂၄ နာရီ
+      <h3 class="subtitle text-center">09-751290444 ကို ၂၄ နာရီ
         အချိန်မရွေး viber ကဖြစ်စေ ဖုန်းနဲ့ဖြစ်စေ ဆက်သွယ်မေးမြန်းနိုင်ပါတယ်</h3>
     </section>
 
