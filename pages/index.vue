47037d75c0e00a06a9a6115f96d7847157895d1e
burmaob <156067621+burmaob@users.noreply.github.com>
2024-01-10 12:17:33 +0000

    initial changes

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,15 @@
 <template>
   <main>
     <section class="self-center flex flex-col flex-1 items-center justify-center">
-      <h1 class="title text-center">Nuxt — Tailwind — Netlify CMS</h1>
-      <h2 class="subtitle text-center">Boilerplate</h2>
+      <h1 class="title text-center">ဘားမားအွန်လိုင်းဘတ်တင်</h1>
+      <h2 class="subtitle text-center">ibet789, Sports899, 555mix, Shan234 များကို စိတ်ချယုံကြည်စွာနဲ့ ကစားနိုင်ပါတယ်</h2>
+      <h3 class="subtitle text-center">မလောင်းတတ် မကစားတတ်လည်း စိတ်ရှည်လက်ရှည် ရှင်းပြပေးမှာမလို့ 09-751290444 ကို ၂၄ နာရီ
+        အချိန်မရွေး viber ကဖြစ်စေ ဖုန်းနဲ့ဖြစ်စေ ဆက်သွယ်မေးမြန်းနိုင်ပါတယ်</h3>
     </section>
 
     <section class="mt-8">
-      <h3 class="text-primary-600 dark:text-primary-400 max-w-5xl mx-auto">Latest blog post</h3>
-      <posts post-type="blog" :amount="1" />
+      <h3 class="text-primary-600 dark:text-primary-400 max-w-5xl mx-auto">ပွဲကြို သုံးသပ်ချက်များ ဖတ်ရန်</h3>
+      <posts post-type="blog" :amount="3" />
     </section>
   </main>
 </template>
