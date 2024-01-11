088418a618ca1cea43ff39c61f29b646eca59df4
burmaob <156067621+burmaob@users.noreply.github.com>
2024-01-11 05:06:22 +0000

    copy from original file

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,7 +1,7 @@
 <template>
   <main>
     <section v-if="posts" class="w-full max-w-5xl mx-auto">
-      <h1 class="title">ဆက်သွယ်ရန်</h1>
+      <h1 class="title">Projects</h1>
       <posts post-type="projects" :amount="10" />
     </section>
   </main>
